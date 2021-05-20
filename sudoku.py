549bdd7b2f5424010f22d4dcc14d97d7c118223a
SRCinVA <scitarella@hotmail.com>
2021-05-19 21:33:28 -0400

    Relatively easy so far

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,14 @@
 def find_next_empty(puzzle):
-
+    # negative spaces are being represented with -1
+    # will return based on column and row indices
+    # if there are no empyty spaces, you return a tuple (None, None)
+    # indices are 0-8
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1
+            return r,c  # because you want to fill it
+    #"else"
+    return None, None # which woudl mean that you're finished.
 
 
 def solve_sudoku(puzzle):
@@ -8,4 +17,4 @@ def solve_sudoku(puzzle):
     # possibly: as you try numbers, you change the list to relfect that you've found a correct one.
     # overall, it sounds like a brute force technique of possible numbers 
 
-row, col = find_next_empty():
+row, col = find_next_empty(puzzle):
