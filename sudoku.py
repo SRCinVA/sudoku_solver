18b184df48ec971dcd126bab5ce1b3b13dbd7fdb
SRCinVA <scitarella@hotmail.com>
2021-05-21 20:59:36 -0400

    So far so good

--- sudoku.py
+++ sudoku.py
@@ -5,10 +5,10 @@ def find_next_empty(puzzle):
     # indices are 0-8
     for r in range(9):
         for c in range(9):
-            if puzzle[r][c] == -1
-            return r,c  # because you want to fill it
+            if puzzle[r][c] == -1:
+                return r,c  # because you want to fill it
     #"else"
-    return None, None # which woudl mean that you're finished.
+    return None, None # which would mean that you're finished.
 
 
 def solve_sudoku(puzzle):
@@ -17,4 +17,11 @@ def solve_sudoku(puzzle):
     # possibly: as you try numbers, you change the list to relfect that you've found a correct one.
     # overall, it sounds like a brute force technique of possible numbers 
 
-row, col = find_next_empty(puzzle):
+    row, col = find_next_empty(puzzle)
+    # now perform a validation check
+    if row is None: # I believe if could just as easily have been column
+        return True # this means that we have solved the puzzle.
+    # if we are not finished, then we need to continue to provide guesses (between 1 and 9)
+    for guess in range(1,10):
+        # now we check the validity
+        if is_valid(puzzle, guess, row, col):
