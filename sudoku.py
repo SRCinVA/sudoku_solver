2bc2f31576e289964a664f406b31b36a77fb4831
SRCinVA <scitarella@hotmail.com>
2021-05-23 21:48:27 -0400

    Struggles a bit at first

--- sudoku.py
+++ sudoku.py
@@ -12,18 +12,24 @@ def find_next_empty(puzzle):
 
 def is_valid(puzzle, guess, row, col):
     # if valid, returns True; if not, returns False
-    # remember sudoki rules: only one occurence of each number.
+    # sudoku rules: only one occurence of each number.
     # let's start with rows, because they're easy:
     row_vals = puzzle[row]
     if guess in row_vals:  # if already present ...
         return False       # ... then it's not valid
+    
     # columns are harder, because they're indexes in given rows
     col_vals = [] # create an empty list
     # for i in range:
         # col_vals.append(puzzle[i][col]) # just need to note the row, col as a tuple (?)
     col_vals = [puzzle[i][col] for i in range(9)] # take puzzle, index into i, then index into col, and do that for i in range (9) 
-    if guess in col_vals:
+    if guess in col_vals:   # don't completely understand the above--shouldn't it just be "row"?
+        # if the guess is in those values, then we return False, because the number has been used.
         return False
+
+    # so far, we've just been figuring out what has been used. Now we need where they are located in the 3*3 grid. (it's 9 * 9 spaces for the whole board)
+    row_start = (row // 3)  # remember this: division and then dispose of remainder to tell you which row
+                            # the given index is on
 
 def solve_sudoku(puzzle):
     # the "puzzle" is a list of lists that we'll solve using backtracking
