80e2ade3a9466198c82e0bb709e6fee8456fbe4d
SRCinVA <scitarella@hotmail.com>
2021-06-02 21:35:25 -0400

    Probably finished ... ?

--- sudoku.py
+++ sudoku.py
@@ -76,3 +76,21 @@ def solve_sudoku(puzzle):
             # ultimately, we'll be trying every possible combination for this puzzle.
 
             # if none of the combinations work, then this puzzle is unsolvable.
+            return False
+
+        if __name__ == '__main__':
+            example_board = [
+                [ 3, 9,-1     -1, 5, -1,   -1, -1, -1],
+                [-1,-1,-1,     2, -1, -1,  -1, -1, 5],
+                [-1,-1,-1,     2, -1, -1,   -1, 8, -1],
+
+                [-1, 5,-1,    -1, 6, 8,     -1, -1,-1],
+                [ 2,-1, 6,    -1, -1, 3,    -1, -1,-1],
+                [-1,-1,-1,    -1,-1,-1,     -1, -1, 4],
+
+                [5, -1, -1,   -1, -1, -1    -1, -1, -1],
+                [5, -1, -1,   -1, -1, -1,    -1, -1,-1],
+                [1, -1,  9,   -1, -1, -1,    2, -1, -1]
+            ]
+            print(solve_sudoku(example_board))
+            print(example_board)
